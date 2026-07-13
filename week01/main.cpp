d8fda3ab6893e66a9c3ae70611224838698e0642
Dong-In Lee <joshuamin2014@gmail.com>
2026-07-13 15:03:30 +0900

    Update starter code

--- week01/main.cpp
+++ week01/main.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
 
 int main() {
-    int quantity = 0;
+    /* TODO: 다음이 화면에 출력되게 해보세요:
 
-    std::cout << "Welcome to C++ Cafe\n";
-    std::cout << "How many coffees would you like? ";
-    std::cin >> quantity;
+    ================================
+                C++ Kiosk
+    ================================
+    
+    Welcome to C++ Cafe!
 
-    if (quantity <= 0) {
-        std::cout << "Invalid quantity\n";
-    } else {
-        int price = 3000;
-        int total = quantity * price;
-
-        std::cout << "Coffee x " << quantity << '\n';
-        std::cout << "Total: " << total << " KRW\n";
-    }
+    */
 
     return 0;
 }
